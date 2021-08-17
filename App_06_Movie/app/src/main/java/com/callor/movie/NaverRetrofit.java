2045af2947b74c10b9f3e16a879beb8a0bb8891e
kisoo6203 <kisoo6203@naver.com>
2021-08-17 15:44:26 +0900

    파일업로드_재시도

---
+++ App_06_Movie/app/src/main/java/com/callor/movie/NaverRetrofit.java
@@ -0,0 +1,15 @@
+package com.callor.movie;
+
+import com.callor.movie.model.NaverParent;
+
+import retrofit2.Call;
+
+public interface NaverRetrofit {
+
+    public Call<NaverParent> getMovies(
+            String clientId,
+            String clientSecret,
+            
+    )
+
+}
